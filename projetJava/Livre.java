00644bc0af2741ef0b08919bf067ff75cfab2b30
Momar <128383510+Papamomars@users.noreply.github.com>
2024-04-14 23:41:22 +0000

    Update Livre.java

--- projetJava/Livre.java
+++ projetJava/Livre.java
@@ -1,1 +1,64 @@
+package projetJava;
+
+
+
+class Livre {
+     String titre;
+     String auteur;
+     int anneePublication;
+     String ISBN;
+
+    public Livre(String titre, String auteur, int anneePublication, String ISBN) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.anneePublication = anneePublication;
+        this.ISBN = ISBN;
+    }
+
+    // Getters and setters
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public int getAnneePublication() {
+        return anneePublication;
+    }
+
+    public void setAnneePublication(int anneePublication) {
+        this.anneePublication = anneePublication;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", anneePublication=" + anneePublication +
+                ", ISBN='" + ISBN + '\'' +
+                '}';
+    }
+}
+
+
+
 
